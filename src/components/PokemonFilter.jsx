127b683e5df064be1f12afc06e1dbbd052eef808
unknown <marcelobettini@yahoo.com.ar>
2022-05-13 11:19:54 -0300

    minor improvements

--- src/components/PokemonFilter.jsx
+++ src/components/PokemonFilter.jsx
@@ -3,12 +3,11 @@ import { useContext } from "react";
 import { TextField } from "@mui/material";
 
 const PokemonFilter = () => {
-    const { filter, setFilter } = useContext(PokemonContext)
+    const { setFilter } = useContext(PokemonContext)
 
     return < TextField
         sx={{ width: '20ch' }}
         label="Search..." variant="filled"
-        value={filter}
         onChange={(e) => setFilter(e.target.value)}
     />
 }
